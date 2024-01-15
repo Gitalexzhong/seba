768b5f4afccde1c6a3a525e55ab3419d96e5b85f
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2024-01-16 00:37:35 +1100

    updated again

--- modules/latenights.js
+++ modules/latenights.js
@@ -28,7 +28,7 @@ function show(guild) {
     return new CronJob(`00 ${i} ${H} * * *`, async () => {
         // Edit the guild icon
         // await guild.setIcon('./static/cursed_logo.png').catch(console.error);
-        await guild.setIcon('./static/ausdaynight.png').catch(console.error);
+        await guild.setIcon('./static/christmas3.png').catch(console.error);
 
         // Add wenkiss emoji
         await guild.emojis.create('./static/wenkiss.png', 'wenkiss').catch(console.error);
